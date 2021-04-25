11dc63c4cd2b53fe5f62461c63725486bb212ede
debasmita97 <73042217+debasmita97@users.noreply.github.com>
2021-04-25 17:45:13 +0530

    Create SolutionsquareIsWhite.java

---
+++ SolutionsquareIsWhite.java
@@ -0,0 +1,43 @@
+/*****
+You are given coordinates, a string that represents the coordinates of a square of the chessboard.
+
+
+Return true if the square is white, and false if the square is black.
+
+The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first, and the number second.
+
+ 
+
+Example 1:
+
+Input: coordinates = "a1"
+Output: false
+Explanation: From the chessboard above, the square with coordinates "a1" is black, so return false.
+Example 2:
+
+Input: coordinates = "h3"
+Output: true
+Explanation: From the chessboard above, the square with coordinates "h3" is white, so return true.
+Example 3:
+
+Input: coordinates = "c7"
+Output: false
+*****/
+
+class SolutionsquareIsWhite {
+    public boolean squareIsWhite(String coordinates) {
+
+        int column = 0;
+
+        
+        //column = hMap.get(coordinates.charAt(0)).intValue();
+        column = coordinates.charAt(0)-'a';
+        System.out.println(column);
+               if(((column%2==0)&&((coordinates.charAt(1))-1)%2==0)|| ((column%2!=0)&&((coordinates.charAt(1))-1)%2!=0))
+               return false; 
+               else 
+               return true;
+                
+            
+    }
+}
